1a256bf214cc75c7dcf7a445de8040df8dea32c3
Bucha Kai <107943861+Kunbucha@users.noreply.github.com>
2022-05-22 19:24:25 +0800

    把example里的pattern从中台里拿出来：它可能是个单独的代码库 repo

---
+++ cp-ddd-example/order-center-pattern/src/main/java/org/example/cp/oms/pattern/SkyworthPattern.java
@@ -0,0 +1,21 @@
+package org.example.cp.oms.pattern;
+
+import org.x.cp.ddd.annotation.Pattern;
+import org.example.cp.oms.spec.model.IOrderModel;
+import org.x.cp.ddd.ext.IIdentityResolver;
+
+import javax.validation.constraints.NotNull;
+
+@Pattern(code = SkyworthPattern.CODE, name = "创维业务模式")
+public class SkyworthPattern implements IIdentityResolver<IOrderModel> {
+    public static final String CODE = "skyworth";
+
+    @Override
+    public boolean match(@NotNull IOrderModel model) {
+        if (model.getCustomerNo() == null) {
+            return false;
+        }
+
+        return model.getCustomerNo().equals("skyworth");
+    }
+}
